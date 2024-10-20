da9e876bd218ceac73c2ccbddce7b32e7a07199b
brianchong01 <129910656+brianchong01@users.noreply.github.com>
2024-10-20 15:55:59 +0800

    Resolved conflict

--- WEB-INF/classes/CreatePublicRoomServlet.java
+++ WEB-INF/classes/CreatePublicRoomServlet.java
@@ -68,12 +68,20 @@ public class CreatePublicRoomServlet extends HttpServlet {
 
                         // Step 3: Execute a SQL SELECT query
                         String sqlStr1 = "INSERT INTO active_room (user_id, username, room_name) VALUES (?, ?, ?);"; // Single-quote
+=======
+                        String sqlStr1 = "INSERT INTO active_room (user_id, username, public, room_name) VALUES (?, ?, ?, ?);"; // Single-quote
+>>>>>>> Stashed changes
                         PreparedStatement preparedStatement = conn.prepareStatement(sqlStr1);
 
                         // Set parameters for the prepared statement
                         preparedStatement.setString(1, userId);
                         preparedStatement.setString(2, userName);
                         preparedStatement.setString(3, sessionName);
+=======
+                        preparedStatement.setBoolean(3, true);
+                        preparedStatement.setString(4, sessionName);
+
+>>>>>>> Stashed changes
                         preparedStatement.executeUpdate(); // Send the query to the server
 
                         // Redirect to video-room.html
